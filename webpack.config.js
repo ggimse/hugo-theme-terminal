59b3714233749c7e2a1d2b1d35b2314040eadebc
Geoffrey Gimse <geoff@textandhubris.com>
2022-03-20 12:03:38 -0500

    Added Nord Coloring

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ module.exports = (env, { mode }) => ({
     blue: join("assets", "css", "color", "blue.css"),
     green: join("assets", "css", "color", "green.css"),
     pink: join("assets", "css", "color", "pink.css"),
+    nord: join("assets", "css", "color", "nord.css"),
   },
   output: {
     filename: "[name].js",
